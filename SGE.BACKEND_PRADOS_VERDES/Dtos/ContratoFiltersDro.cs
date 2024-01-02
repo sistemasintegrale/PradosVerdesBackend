db6158f2638b67c7dfae6575a7e7a73607cf068d
Rafhael <rafhaelpillaca2001@gmail.com>
2024-01-02 12:39:53 -0500

    d

--- SGE.BACKEND_PRADOS_VERDES/Dtos/ContratoFiltersDro.cs
+++ SGE.BACKEND_PRADOS_VERDES/Dtos/ContratoFiltersDro.cs
@@ -5,5 +5,7 @@
         public string fechaInicio { get; set; }
         public string fechaFinal { get; set; }
         public int? vendc_icod_vendedor { get; set; }
+        public string numContrato { get; set; } = string.Empty;
+        public string contratante { get; set; } = string.Empty;
     }
 }
--- SGE.BACKEND_PRADOS_VERDES/Filters/ContratoFilter.cs
+++ SGE.BACKEND_PRADOS_VERDES/Filters/ContratoFilter.cs
@@ -5,5 +5,7 @@
         public DateTime fechaInicio { get; set; }
         public DateTime fechaFinal { get; set; }
         public int? vendc_icod_vendedor { get; set; }
+        public string numContrato { get; set; } = string.Empty;
+        public string contratante { get; set; } = string.Empty;
     }
 }
